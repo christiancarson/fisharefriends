6929e05608f5ee6e4e6f176453b445525ab89f6c
Christian Carson <60585941+christiancarson@users.noreply.github.com>
2026-09-23 13:15:37 -0700

    sync uses the python that has pillow

--- sync.sh
+++ sync.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+set -e
 cd "$(dirname "$0")"
 mkdir /tmp/fisharefriends.lock 2>/dev/null || exit 0
 trap 'rmdir /tmp/fisharefriends.lock' EXIT
-/usr/bin/python3 import.py
+/opt/homebrew/Caskroom/miniconda/base/bin/python3 import.py
 ./test.sh
 git add -A
 git diff --cached --quiet || git commit -qm "photos $(date +%Y-%m-%d)"
