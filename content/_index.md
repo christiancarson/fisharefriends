d22c8538a28151fb31237aa42e6901a51bf3369b
Christian Carson <60585941+christiancarson@users.noreply.github.com>
2026-09-23 20:39:46 -0700

    water link back in the intro, shorter donation line

--- content/_index.md
+++ content/_index.md
@@ -5,9 +5,9 @@ Hello!
 
 You’ve come upon my lie (place where a fish rests).
 
-Here I share a {{< tab journal >}}monthly photo journal{{< /tab >}} from my time on the water.
+Here I share a {{< tab journal >}}monthly photo journal{{< /tab >}} from my time on the {{< tab water >}}water{{< /tab >}}.
 
-It is also a place where you can {{< tab trips >}}sign up to join me on a trip{{< /tab >}} and learn the basics of fly fishing - 100% for free with no gear required. If you so choose, there will be an opportunity to donate to organizations that do restoration on the waters we end up fishing.
+It is also a place where you can {{< tab trips >}}sign up to join me on a trip{{< /tab >}} and learn the basics of fly fishing - 100% for free with no gear required. If you so choose, there will be an opportunity to donate to organizations that do restoration on the waters we fish.
 
 See you on the water:)
 
--- test.sh
+++ test.sh
@@ -69,7 +69,7 @@ grep -q '<p class="tags"><a href="/categories/fish/">fish</a><a href="/categorie
 ! grep -qi 'gab and gob\|mack the bulltrout' public/categories/fish/index.html
 grep -q 'data-signup="https://script.google.com/macros/s/AKfycbx4_QPAM9AQnIfD8QtnuWdZJgYkNLEla_AJtjgrlUJJ_Pu4q_gHgVZ0Ey3dX426zPyGSw/exec"' public/trips/squamish-river/index.html
 grep -q 'come upon my lie' public/index.html
-grep -q '<a class="tablink" href="/journal/" data-link="journal" style="--hue: 48">monthly photo journal</a> from my time on the water.' public/index.html
+grep -q '<a class="tablink" href="/journal/" data-link="journal" style="--hue: 48">monthly photo journal</a> from my time on the <a class="tablink" href="/categories/water/" data-link="water" style="--hue: 215">water</a>.' public/index.html
 grep -q '<a class="tablink" href="/trips/[a-z-]*/" data-link="trips" style="--hue: 130">sign up to join me on a trip</a>' public/index.html
 ! grep -q '>all</a>' public/index.html
 grep -q 'id="random" href="/posts/">go fish <svg class="doodle" aria-hidden="true"><use href="#fish"/></svg></a>' public/index.html
